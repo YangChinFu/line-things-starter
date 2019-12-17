a5fcf47b90fb171c4dec3a1ccc431ffd775ce048
Fu <asd5242539@gmail.com>
2019-12-18 01:30:36 +0800

    debug

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -190,12 +190,14 @@ function liffGetUserService(service) {
     service.getCharacteristic(ON_CONNECT_HOOK_CHARACTERISTIC_UUID).then(characteristic => {
         liffGetLedStateCharacteristic(characteristic);
     }).catch(error => {
+        alert(ON_CONNECT_HOOK_CHARACTERISTIC_UUID);
         uiStatusError(makeErrorMsg(error), false);
     });
     // Toggle LED
     service.getCharacteristic(LED_CHARACTERISTIC_UUID).then(characteristic => {
         window.ledCharacteristic = characteristic;
     }).catch(error => {
+        alert(LED_CHARACTERISTIC_UUID);
         uiStatusError(makeErrorMsg(error), false);
     });
 }
