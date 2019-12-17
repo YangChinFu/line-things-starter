9ae6a45b4548f5184e121c978e526eacbb83b561
Fu <asd5242539@gmail.com>
2019-12-18 00:30:43 +0800

    fix bug

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -186,13 +186,6 @@ function liffConnectToDevice(device) {
 }
 
 function liffGetUserService(service) {
-    // Button pressed state
-    service.getCharacteristic(BTN_CHARACTERISTIC_UUID).then(characteristic => {
-        liffGetButtonStateCharacteristic(characteristic);
-    }).catch(error => {
-        uiStatusError(makeErrorMsg(error), false);
-    });
-
     // Toggle LED
     service.getCharacteristic(LED_CHARACTERISTIC_UUID).then(characteristic => {
         window.ledCharacteristic = characteristic;
